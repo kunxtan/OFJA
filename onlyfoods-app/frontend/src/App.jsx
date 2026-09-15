3aec432070f154818093d5f70402aa6cb6ffae06
Pattara. <kunxtan@gmail.com>
2026-09-15 20:08:35 +0700

    eiei

--- onlyfoods-app/frontend/src/App.jsx
+++ onlyfoods-app/frontend/src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
   const [authForm, setAuthForm] = useState({ username: '', password: '', name: '' });
   const [authError, setAuthError] = useState('');
 
-  const API_BASE = "";
+  const API_BASE = "[http://onlyfoods.cskmitl.com:3306](http://onlyfoods.cskmitl.com:3306)";
 
   useEffect(() => {
     fetchFoodCourtStatus();
